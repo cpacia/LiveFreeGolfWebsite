e07c70fb7d3f73d8b782f6491b78919a47cbdb33
Chris Pacia <ctpacia@gmail.com>
2025-06-04 11:10:45 -0400

    Add additional sponsor

--- src/components/SponsorCarousel.jsx
+++ src/components/SponsorCarousel.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import './SponsorsCarousel.css';
 
-// Example sponsor logos imported or referenced from /public
 const sponsors = [
-  '/images/FASTENAL.png',
-  '/images/izzo.png',
-  '/images/impact-fire.png',
-];
-const links = [
-  'https://www.fastenal.com/',
-  'https://impactfireservices.com/',
-  'https://izzo.com',
+  { src: '/images/FASTENAL.png', link: 'https://www.fastenal.com/' },
+  { src: '/images/impact-fire.png', link: 'https://impactfireservices.com/' },
+  { src: '/images/izzo.png', link: 'https://izzo.com' },
+  { src: '/images/bnice.avif', link: 'https://bnicedetailing.com/' },
 ];
 
 export default function SponsorsCarousel() {
@@ -18,14 +13,14 @@ export default function SponsorsCarousel() {
     <section className="sponsors-carousel">
       
       <div className="sponsors-track">
-        {sponsors.map((src, i) => (
-          <div key={i} className="sponsor-item">
-            <a href={links[i]} target="_blank" rel="noopener noreferrer">
-            	<img src={src} alt={`Sponsor ${i + 1}`} />
-            </a>
-          </div>
-        ))}
-      </div>
+        {sponsors.map(({ src, link }, i) => (
+			<div key={i} className="sponsor-item">
+				<a href={link} target="_blank" rel="noopener noreferrer">
+					<img src={src} alt={`Sponsor ${i + 1}`} />
+				</a>
+			</div>
+		))}
+		</div>
     </section>
   );
 }
--- src/components/SponsorsCarousel.jsx
+++ src/components/SponsorsCarousel.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import './SponsorsCarousel.css';
 
-// Example sponsor logos imported or referenced from /public
 const sponsors = [
-  '/images/FASTENAL.png',
-  '/images/impact-fire.png',
-  '/images/izzo.png',
-];
-
-const links = [
-  'https://www.fastenal.com/',
-  'https://impactfireservices.com/',
-  'https://izzo.com',
+  { src: '/images/FASTENAL.png', link: 'https://www.fastenal.com/' },
+  { src: '/images/impact-fire.png', link: 'https://impactfireservices.com/' },
+  { src: '/images/izzo.png', link: 'https://izzo.com' },
+  { src: '/images/bnice.avif', link: 'https://bnicedetailing.com/' },
 ];
 
 export default function SponsorsCarousel() {
@@ -19,13 +13,13 @@ export default function SponsorsCarousel() {
     <section className="sponsors-carousel">
       <h2>Sponsors</h2>
       <div className="sponsors-track">
-        {sponsors.map((src, i) => (
-          <div key={i} className="sponsor-item">
-            <a href={links[i]} target="_blank" rel="noopener noreferrer">
-            	<img src={src} alt={`Sponsor ${i + 1}`} />
-            </a>
-          </div>
-        ))}
+        {sponsors.map(({ src, link }, i) => (
+		  <div key={i} className="sponsor-item">
+			<a href={link} target="_blank" rel="noopener noreferrer">
+			  <img src={src} alt={`Sponsor ${i + 1}`} />
+			</a>
+		  </div>
+		))}
       </div>
     </section>
   );
